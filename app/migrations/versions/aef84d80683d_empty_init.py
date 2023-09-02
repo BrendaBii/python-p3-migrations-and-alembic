79f75ab4e0535dd5790563e0fd3794d344f71c11
BrendaBii <bii.brenda.bcb@gmail.com>
2023-09-02 16:19:09 +0300

    Completed assignment.

---
+++ app/migrations/versions/aef84d80683d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: aef84d80683d
+Revises: 
+Create Date: 2023-09-02 16:14:33.746802
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'aef84d80683d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
